ad35ffd1709d8955980f5828355bfc0a0cba797d
Amanda yang <46439448+AmandaYang22@users.noreply.github.com>
2019-01-08 05:15:45 -0600

    Add files via upload

---
+++ checkname.php
@@ -0,0 +1,19 @@
+<?php
+	$con=mysql_connect('localhost','root','');
+	$db_selected=mysql_select_db('n152127',$con);
+	mysql_query("set names gbk");
+	$name=$_GET['id'];
+	//echo $name;
+	//$sql="select * from user where username='$name'";
+	$sql="select * from user where  username='".$name."'";
+	$result=mysql_query($sql,$con);
+	$data=mysql_num_rows($result);
+	//echo "data:";
+	//echo $data;
+	if($data){
+		echo "<font color=red>用户名已存在</font>";
+
+	}else{
+		echo "<font color=green>用户名可以使用</font>";
+	}
+?>
